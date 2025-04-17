ecfc1d101a53decfbe8d05679c632f0e3c163117
LucasBuccini <lucasmbuccini@gmail.com>
2025-04-17 16:31:12 -0300

    trying chat gt response

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors"
 import dotenv from "dotenv"
 import { sendWhatsappMessage } from "./services/twilio";
+import { getOpenAICompletion } from "./services/openai";
 
 
 const app = express()
@@ -17,7 +18,8 @@ app.post('/chat/send', async (req,res)=>{
     const {to, body} = req.body
     try{
         await sendWhatsappMessage(`whatsapp:${to}`, body)
-        res.status(200).json({success: true, body})
+        const completion = await getOpenAICompletion(body)
+        res.status(200).json({success: true, completion})
     }catch(error){
         res.status(500).json({success: false, error})
     }
